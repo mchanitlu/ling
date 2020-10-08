eaea88027a58d2f5ee9e79c6cf11591a419518a4
mchanitlu <56201906+mchanitlu@users.noreply.github.com>
2020-10-08 04:28:29 -0400

    Update segmenter.py

--- segmenter.py
+++ segmenter.py
@@ -2,9 +2,6 @@ import sys
 text = sys.stdin.read()
 for c in text:
         if c == '.':
-                text = text.replace('. ','\n')
+                text = text.replace('. ','.\n')
 print(text)
-
-
-
 
