f2636efbe9933e8d5577bd2286d5012af68eaeb2
mchanitlu <56201906+mchanitlu@users.noreply.github.com>
2020-10-10 23:43:11 -0400

    Update segmenter.py

--- segmenter.py
+++ segmenter.py
@@ -3,9 +3,15 @@ text = sys.stdin.read()
 for c in text:
         if c == '.':
                 text = text.replace('. ','.\n')
-        if c == ")":
-                text = text.replace('.) ','.)\n')
+        if c == ')':
+                text = text.replace('.) ','.\n')
+        if c == '?':
+                text = text.replace('? ','.\n')
+        if c == '!':
+                text = text.replace('! ','.\n')
 print(text)
+
+
 
 
 
