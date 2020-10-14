5149f17ef1e1188282cc5ec72b66c7d8f3d5b21c
mchanitlu <56201906+mchanitlu@users.noreply.github.com>
2020-10-14 13:44:07 -0400

    Delete Tokenisation.py

--- Tokenisation.py
+++
@@ -1,8 +0,0 @@
-import sys
-text = sys.stdin.read()
-z = text.split()
-print(z)
-a = "ID\tFROM\tLEMMA\tUPOS\tXPOS\tFEATS\tHEAD\tDEPREL\tDEPS\tMISC"
-print(a)
-for i in range(len(z)):
-        print("%d\t%s\t-\t-\t-\t-\t-\t-\t-"%(i+1,z[i]))
